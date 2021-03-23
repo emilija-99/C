25b398902d26f5214080d8213e306e5178dd4940
Nobody <76665282+emilija-99@users.noreply.github.com>
2021-03-23 07:26:04 -0400

    Add files via upload

---
+++ primer2.c
@@ -0,0 +1,19 @@
+//Niz od vise razmaka zamenjuje jednim razmakom
+#include <stdio.h>
+#define NONBLANK 'a'
+int main(){
+	
+	int c, lastc;
+	
+	lastc = NONBLANK;
+	
+	while((c = getchar()) != EOF){
+			if(c != ' ')
+				putchar(c);
+			if(c == ' ')
+				if(lastc != ' ')
+					putchar(c);
+			lastc = c;
+	}
+}
+	
